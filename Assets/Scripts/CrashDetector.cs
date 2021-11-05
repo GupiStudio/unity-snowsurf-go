51dfecee8be9301ceea89393dc6fcce0c6df029b
wekonu <wahjoe.eko.n@gmail.com>
2021-11-05 22:32:26 +0700

    [mod] win-lose reload scene delay using invoke

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -5,12 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
+    [SerializeField] float _reloadDelay = 1f;
+
     void OnTriggerEnter2D(Collider2D other) 
     {
         if (other.CompareTag("Ground"))
         {
             Debug.Log("You died!");
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            Invoke("ReloadScene", _reloadDelay);
         }
+    }
+
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
--- Assets/Scripts/FinishDetector.cs
+++ Assets/Scripts/FinishDetector.cs
@@ -5,12 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class FinishDetector : MonoBehaviour
 {
+    [SerializeField] float _reloadDelay = 1f;
+    
     void OnTriggerEnter2D(Collider2D other) 
     {
         if (other.CompareTag("Player"))
         {
             Debug.Log("You finished!");
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            Invoke("ReloadScene", _reloadDelay);
         }    
+    }
+
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
