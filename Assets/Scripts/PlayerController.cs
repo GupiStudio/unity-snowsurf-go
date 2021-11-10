3aef0ce4539c9c782359b0bf498c31bcbd4d8a54
wekonu <wahjoe.eko.n@gmail.com>
2021-11-10 21:48:49 +0700

    [mod] implement boost player speed when arrow up key pressed

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,16 +6,45 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float _torqueAmount = 1f;
+    [SerializeField] float baseSpeed = 20f;
+    [SerializeField] float boostSpeed = 30f;
+    [SerializeField] float boostHp = 10f;
 
     Rigidbody2D rb2d;
+    SurfaceEffector2D surfaceEffector2D;
 
     void Start()
     {
-        rb2d = GetComponent<Rigidbody2D>();   
+        rb2d = GetComponent<Rigidbody2D>();
+        surfaceEffector2D = FindObjectOfType<SurfaceEffector2D>();
+        surfaceEffector2D.speed = baseSpeed;
     }
 
     // Update is called once per frame
     void Update()
+    {
+        RotatePlayer();
+        RespondToBoost();
+    }
+
+    private void RespondToBoost()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+
+            if (boostHp >= 0)
+            {
+                surfaceEffector2D.speed = boostSpeed;
+                boostHp -= 1 * Time.deltaTime;
+
+                Debug.Log("Boost HP: " + boostHp);
+            }
+        }
+        else
+            surfaceEffector2D.speed = baseSpeed;
+    }
+
+    void RotatePlayer()
     {
         if (Input.GetKey(KeyCode.LeftArrow))
             rb2d.AddTorque(_torqueAmount);
